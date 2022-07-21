0e0d867bc4719c2c7ba40436858260829d393ff0
D L <2127586@cognizant.com>
2022-07-21 13:56:22 +0530

    Junit test cases

--- HelloWorld/src/test/java/com/example/HelloWorld/HelloWorldApplicationTests.java
+++ HelloWorld/src/test/java/com/example/HelloWorld/HelloWorldApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.HelloWorld;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class HelloWorldApplicationTests {
 
+	@Autowired
+	HelloWorldController helloWorldController;
+
+	@Autowired
+	HelloWorldApplication helloWorldApplication;
+
 	@Test
-	void contextLoads() {
+	void testingHelloWorld() {
+		assertEquals("Hello World",
+				helloWorldController.sayHello());
+	}
+
+	@Test
+	void testingHeyWorld() {
+		assertEquals("Hey World",
+				helloWorldApplication.sayHello());
 	}
 
 }
